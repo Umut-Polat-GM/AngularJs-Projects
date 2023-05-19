d9ffffcf0f759d6e991941ba635e0dffcdf775b3
Umut Polat <62594144+Umut-Polat-GM@users.noreply.github.com>
2023-05-19 18:01:56 +0300

    chore: todoapp finish |Angular Js

--- todoApp/src/app/app.component.ts
+++ todoApp/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Modal, TodoItem } from './modal';
 
 @Component({
   selector: 'app-root',
@@ -6,12 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todoApp';
-  user="Umut Polat";
-  items= [
-    {description: "Kahvaltı", action: "No"},
-    {description: "Sinama", action: "Yes"},
-    {description: "Ders Çalışma", action: "No"},
-    {description: "Yüzme", action: "Yes"},
-  ];
+  model = new Modal();
+  isDisplay=false;
+
+  getName() {
+    return this.model.user;
+  }
+
+  getItems() {
+    if(this.isDisplay){
+      return this.model.items;
+    }
+    return this.model.items.filter(item => !item.action);
+  }
+
+  addItem(value:string){
+    if(value !=""){
+      this.model.items.push(new TodoItem(value, false))
+    }
+  }
 }
